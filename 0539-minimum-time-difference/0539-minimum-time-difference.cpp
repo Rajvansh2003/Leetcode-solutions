32ad206353115cbafa91468240628a83f87a15d9
Rajvansh Sharma <114468550+Rajvansh2003@users.noreply.github.com>
2024-09-16 23:05:52 +0530

    Time: 13 ms (53.24%), Space: 17.3 MB (95.56%) - LeetHub

---
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findMinDifference(vector<string>& timePoints) {
+        vector<int> minutes;
+        for(int i = 0; i<timePoints.size(); i++){
+            string curr = timePoints[i];
+
+            int hours = stoi(curr.substr(0,2));
+            int minute = stoi(curr.substr(3,2));
+            int totalMinutes = hours * 60 + minute;
+            minutes.push_back(totalMinutes);
+        }
+
+        sort(minutes.begin(),minutes.end());
+
+        int mini = INT_MAX;
+        int n = minutes.size();
+        for(int i = 0; i<n-1; i++){
+            int diff = minutes[i+1] - minutes[i];
+            mini = min(mini,diff);
+        }
+
+        int lastDif = (minutes[0] + 1440) - minutes[n-1];
+        mini = min(mini,lastDif);
+        return mini;
+    }
+};
