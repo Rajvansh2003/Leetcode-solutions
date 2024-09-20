a8c64ac4cca421ca79f89eb3db68c50986d3ffda
Rajvansh Sharma <114468550+Rajvansh2003@users.noreply.github.com>
2024-09-20 23:21:20 +0530

    Time: 11 ms (27.02%), Space: 9.7 MB (90.93%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        string reversedString = s;
+       
+        reverse(reversedString.begin(), reversedString.end());
+
+      
+        for (int i = 0; i < s.size(); ++i) {
+            
+            if (!memcmp(s.c_str(), reversedString.c_str() + i, s.size() - i)) {
+                
+                return reversedString.substr(0, i) + s;
+            }
+        }
+        
+        return reversedString + s;
+    }
+};
