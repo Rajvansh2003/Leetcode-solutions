73e1b743e136cf3f00a58df887ed2ba3e4bdcc66
Rajvansh Sharma <114468550+Rajvansh2003@users.noreply.github.com>
2024-10-03 22:36:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        int n = nums.size();
+        long long totalSum =
+            accumulate(nums.begin(), nums.end(), 0LL);  
+
+        
+        
+        if (totalSum % p == 0) return 0;
+
+        int minLen = n;  
+
+        
+        for (int start = 0; start < n; ++start) {
+            long long subSum = 0;  
+            for (int end = start; end < n; ++end) {
+                subSum += nums[end];  
+
+                
+                long long remainingSum = (totalSum - subSum) % p;
+
+                if (remainingSum == 0) {
+                    minLen = min(minLen,
+                                 end - start +
+                                     1);  
+                }
+            }
+        }
+
+     
+        return minLen == n ? -1 : minLen;
+    }
+};
